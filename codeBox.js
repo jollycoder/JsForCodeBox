473f05cfea509c354d33d31be68c8e9bffa3f257
jollycoder <dfiveg@gmail.com>
2017-01-26 02:09:57 +0300

    alert('test') delete

--- codeBox.js
+++ codeBox.js
@@ -1,9 +1,8 @@
 "use strict";
 
 var codeBoxes = document.getElementsByClassName('codebox');
-alert(navigator.userAgent + '\n' + navigator.userAgent.search(/Firefox|Chrome/i));
+
 if (navigator.userAgent.search(/Firefox|Chrome/i) == -1) {
-    alert('not ff');
     for (var i = 0; i < codeBoxes.length; i++)
         codeBoxes[i].style = "border: 2px solid #acf; border-radius: 5px; background: #f8f8f8; padding: 12px 5px 5px 15px;";
 }
