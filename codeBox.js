d5b832b0977e791434b823423ac64d732c898bdd
jollycoder <dfiveg@gmail.com>
2017-01-25 21:15:27 +0300

    test

--- codeBox.js
+++ codeBox.js
@@ -1,16 +1,32 @@
 var codeBoxes = document.getElementsByClassName('codebox');
 for(var i=0; i<codeBoxes.length; i++) {
     var box = codeBoxes[i];
+    var getValue;
     var pre = box.getElementsByTagName('pre')[0];
-    var boxPos = box.getBoundingClientRect();
-    var prePos = pre.getBoundingClientRect();
-    var offset = prePos.top - boxPos.top;
+    if (!getValue) {
+        var boxPos = box.getBoundingClientRect();
+        var prePos = pre.getBoundingClientRect();
+        var offsetTop = prePos.top - boxPos.top;
+        getValue = true;
+    }
+    var codeTextHeight = pre.getElementsByTagName('code')[0].offsetHeight;
+    var boxHeight = offsetTop + codeTextHeight;
+    ((boxHeight > 500) && (boxHeight = 500));
+    // alert(offsetTop);
     box.style = 'border: 2px solid #acf; border-radius: 5px; background: #f8f8f8; padding: 12px 5px 5px 15px;' +
-                                                        ' position: relative; resize: vertical; overflow: auto;';
-    setStyle('.entry-content pre', 'position', 'absolute');
-    setStyle('.entry-content pre', 'top', offset + 'px');
-    setStyle('.entry-content pre', 'height', 'calc(100% - ' + offset + 'px)');
+                        'height: ' + boxHeight + 'px; position: relative; resize: vertical; overflow: hidden;';
 }
+
+var computedStyle = getStyle(codeBoxes[0]);
+var padding = +computedStyle.paddingLeft.slice(0, -2) + +computedStyle.paddingRight.slice(0, -2);
+var border = +computedStyle.borderWidth.slice(0, -2)*2;
+
+setStyle('.entry-content pre', 'position', 'absolute');
+setStyle('.entry-content pre', 'top', offsetTop + 'px');
+setStyle('.entry-content pre', 'maxHeight', 'none');
+setStyle('.entry-content pre', 'height', 'calc(100% - ' + (offsetTop + 7) + 'px)');
+setStyle('.entry-content pre', 'paddingRight', '0');
+setStyle('.entry-content pre', 'width', (codeBoxes[0].offsetWidth - padding - border - 5) + 'px');
 
 function setStyle(selector, style, value)  {
     if (document.styleSheets[0].cssRules)
@@ -22,4 +38,8 @@ function setStyle(selector, style, value)  {
         if (rules[i].selectorText == selector)
             rules[i].style[style] = value;
     }
+}
+
+function getStyle(elem) {
+    return window.getComputedStyle ? getComputedStyle(elem, "") : elem.currentStyle;
 }
