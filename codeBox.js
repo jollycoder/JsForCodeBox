7eb8669d07c38e8c249373fad6bbbb88dcd588ca
jollycoder <dfiveg@gmail.com>
2017-01-26 01:23:04 +0300

    новый код только для ff и chrome

--- codeBox.js
+++ codeBox.js
@@ -1,32 +1,42 @@
+"use strict";
+
 var codeBoxes = document.getElementsByClassName('codebox');
-for(var i=0; i<codeBoxes.length; i++) {
-    var box = codeBoxes[i];
-    var getValue;
-    var pre = box.getElementsByTagName('pre')[0];
-    if (!getValue) {
-        var boxPos = box.getBoundingClientRect();
-        var prePos = pre.getBoundingClientRect();
-        var offsetTop = prePos.top - boxPos.top;
-        getValue = true;
-    }
-    var codeTextHeight = pre.getElementsByTagName('code')[0].offsetHeight;
-    var boxHeight = offsetTop + codeTextHeight;
-    ((boxHeight > 500) && (boxHeight = 500));
-    // alert(offsetTop);
-    box.style = 'border: 2px solid #acf; border-radius: 5px; background: #f8f8f8; padding: 12px 5px 5px 15px;' +
-                        'height: ' + boxHeight + 'px; position: relative; resize: vertical; overflow: hidden;';
+
+if (navigator.userAgent.search(/Firefox|Chrome/i) == -1) {
+    for (var i = 0; i < codeBoxes.length; i++)
+        codeBoxes[i].style = "border: 2px solid #acf; border-radius: 5px; background: #f8f8f8; padding: 12px 5px 5px 15px;";
 }
+else {
+    for (i = 0; i < codeBoxes.length; i++) {
+        var box = codeBoxes[i];
+        var getValue;
+        var pre = box.getElementsByTagName('pre')[0];
+        if (!getValue) {
+            var boxPos = box.getBoundingClientRect();
+            var prePos = pre.getBoundingClientRect();
+            var offsetTop = prePos.top - boxPos.top;
+            var computedStyle = getStyle(codeBoxes[0]);
+            var paddingBottom = +computedStyle.paddingBottom.slice(0, -2);
+            getValue = true;
+        }
+        var codeTextHeight = pre.getElementsByTagName('code')[0].offsetHeight;
+        var boxHeight = offsetTop + codeTextHeight;
+        ((boxHeight - paddingBottom > 500) && (boxHeight = 500 + paddingBottom));
 
-var computedStyle = getStyle(codeBoxes[0]);
-var padding = +computedStyle.paddingLeft.slice(0, -2) + +computedStyle.paddingRight.slice(0, -2);
-var border = +computedStyle.borderWidth.slice(0, -2)*2;
+        box.style = 'border: 2px solid #acf; border-radius: 5px; background: #f8f8f8; padding: 12px 5px 5px 15px;' +
+            'height: ' + boxHeight + 'px; position: relative; resize: vertical; overflow: hidden;';
+    }
 
-setStyle('.entry-content pre', 'position', 'absolute');
-setStyle('.entry-content pre', 'top', offsetTop + 'px');
-setStyle('.entry-content pre', 'maxHeight', 'none');
-setStyle('.entry-content pre', 'height', 'calc(100% - ' + (offsetTop + 7) + 'px)');
-setStyle('.entry-content pre', 'paddingRight', '0');
-setStyle('.entry-content pre', 'width', (codeBoxes[0].offsetWidth - padding - border - 5) + 'px');
+    var padding = +computedStyle.paddingLeft.slice(0, -2) + +computedStyle.paddingRight.slice(0, -2);
+    var border = +computedStyle.borderWidth.slice(0, -2) * 2;
+
+    setStyle('.entry-content pre', 'position', 'absolute');
+    setStyle('.entry-content pre', 'top', offsetTop + 'px');
+    setStyle('.entry-content pre', 'maxHeight', 'none');
+    setStyle('.entry-content pre', 'height', 'calc(100% - ' + (offsetTop + 7) + 'px)');
+    setStyle('.entry-content pre', 'paddingRight', '0');
+    setStyle('.entry-content pre', 'width', (codeBoxes[0].offsetWidth - padding - border - 5) + 'px');
+}
 
 function setStyle(selector, style, value)  {
     if (document.styleSheets[0].cssRules)
