b9f9e580f6c2d855c0a70710598a0ebd7887a7e9
jollycoder <dfiveg@gmail.com>
2017-01-26 20:34:24 +0300

    Update codeBox.js

--- codeBox.js
+++ codeBox.js
@@ -1,6 +1,7 @@
 var codeBoxes = document.getElementsByClassName('codebox');
 
 if (navigator.userAgent.search(/Firefox|Chrome/i) == -1) {
+    alert(navigator.userAgent);
     for (var i = 0; i < codeBoxes.length; i++)
         codeBoxes[i].style = "border: 2px solid #acf; border-radius: 5px; background: #f8f8f8; padding: 12px 5px 5px 15px;";
 }
