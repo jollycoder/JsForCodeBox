b298e0b6901321d831b18535701e904366f7a543
jollycoder <dfiveg@gmail.com>
2017-01-26 21:20:27 +0300

    Update codeBox.js

--- codeBox.js
+++ codeBox.js
@@ -1,12 +1,7 @@
 var codeBoxes = document.getElementsByClassName('codebox');
-console.info(navigator.userAgent);
-alert(navigator.userAgent);
-if (navigator.userAgent.search(/Firefox|Chrome/i) == -1) {
-    setStyle('.entry-content .codebox', [['border-radius', '5px']])
-    alert(navigator.userAgent);
-    for (var i = 0; i < codeBoxes.length; i++)
-        codeBoxes[i].style = "border: 2px solid #acf; border-radius: 5px; background: #f8f8f8; padding: 12px 5px 5px 15px;";
-}
+
+if (navigator.userAgent.search(/Firefox|Chrome/i) == -1)
+    setStyle('.entry-content .codebox', [['border', '2px solid #acf'], ['border-radius', '5px'], ['background', '#f8f8f8'], ['padding', '12px 5px 5px 15px']]);
 else {
     for (i = 0; i < codeBoxes.length; i++) {
         var gotValue, box = codeBoxes[i];
