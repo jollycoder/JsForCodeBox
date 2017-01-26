078c55ec66cf7f46f22f204cf83706567e81e7c3
jollycoder <dfiveg@gmail.com>
2017-01-26 06:28:43 +0300

    change rule set

--- codeBox.js
+++ codeBox.js
@@ -31,7 +31,7 @@ else {
 
     setStyle('.entry-content pre', [['position', 'absolute'],
                                     ['maxHeight', 'none'],
-                                    ['height', 'calc(100% - ' + offsetTop + 'px)'],
+                              //      ['height', 'calc(100% - ' + offsetTop + 'px)'],
                                     ['paddingRight', '0'],
                                     ['width', (codeBoxes[0].offsetWidth - padding - border - 5) + 'px']]);
 }
