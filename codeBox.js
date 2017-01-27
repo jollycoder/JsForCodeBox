ed017dca4257fabb9cf85c3b191d9a02e0bb4834
jollycoder <dfiveg@gmail.com>
2017-01-27 17:49:41 +0300

    change code struct

--- codeBox.js
+++ codeBox.js
@@ -17,26 +17,20 @@ if (navigator.userAgent.search(/Firefox|Chrome/i) > -1) {
             var boxPos = box.getBoundingClientRect();
             var prePos = pre.getBoundingClientRect();
             var offsetTop = prePos.top - boxPos.top;
-            var computedStyle = getStyle(codeBoxes[0]);
-            var paddingBottom = +computedStyle.paddingBottom.slice(0, -2);
             gotValue = true;
         }
         var codeTextHeight = pre.getElementsByTagName('code')[0].offsetHeight;
         var boxHeight = offsetTop + codeTextHeight + 5;
         (boxHeight > 500) && (boxHeight = 500);
-
-        box.style = 'height: ' + boxHeight + 'px;';
+        box.style.height = boxHeight + 'px';
     }
-
-    var padding = +computedStyle.paddingLeft.slice(0, -2) + +computedStyle.paddingRight.slice(0, -2);
-    var border = +computedStyle.borderWidth.slice(0, -2) * 2;
 
     setStyle('.entry-content pre', [['position', 'absolute'],
                                     ['maxHeight', 'none'],
                                     ['top', offsetTop + 'px'],
                                     ['bottom', '5px'],
                                     ['paddingRight', '0'],
-                                    ['width', (codeBoxes[0].offsetWidth - padding - border - 5) + 'px']]);
+                                    ['width', (codeBoxes[0].offsetWidth - 20 - 4 - 5) + 'px']]);
 }
 
 function setStyle(selector, rulesArray)  {
@@ -48,8 +42,4 @@ function setStyle(selector, rulesArray)  {
             for (var j = 0; j < rulesArray.length; j++)
                 rules[i].style[rulesArray[j][0]] = rulesArray[j][1];
     }
-}
-
-function getStyle(elem) {
-    return window.getComputedStyle ? getComputedStyle(elem, "") : elem.currentStyle;
 }
