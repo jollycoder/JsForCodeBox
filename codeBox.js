c8775b0ec54af131ce6b2d496e8dee506b61e01e
jollycoder <dfiveg@gmail.com>
2017-01-27 16:50:52 +0300

    change code struct

--- codeBox.js
+++ codeBox.js
@@ -19,6 +19,7 @@ if (navigator.userAgent.search(/Firefox|Chrome/i) > -1) {
             var offsetTop = prePos.top - boxPos.top;
             var computedStyle = getStyle(codeBoxes[0]);
             var paddingBottom = +computedStyle.paddingBottom.slice(0, -2);
+            alert(paddingBottom);
             gotValue = true;
         }
         var codeTextHeight = pre.getElementsByTagName('code')[0].offsetHeight;
