43c16a9ea85cdd3c4b3d9bb341ba7278caffffe7
jollycoder <dfiveg@gmail.com>
2017-01-28 16:50:58 +0300

    change code struct

--- codeBox.js
+++ codeBox.js
@@ -5,6 +5,7 @@ var backgroundColor = '#f8f8f8';
 var maxHeight = 500;
 var fontFamily = 'Droid Sans Mono';
 var paddingRight = 5, paddingBottom = 5, paddingLeft = 15;
+var offsetTop = 47;  // отступ области с кодом от верха codebox
 
 var codeBoxes = document.getElementsByClassName('codebox');
 
@@ -34,7 +35,6 @@ for (i = 0; i < codeBoxes.length; i++) {
     codeText.setAttribute('style', 'font-size: 12.5px !important; font-family: ' + fontFamily + ';');
 
     var boxPos = box.getBoundingClientRect();
-    var offsetTop = 47;
     var codePos = codeText.getBoundingClientRect();
 
     var codeTextHeight = codeText.offsetHeight;
