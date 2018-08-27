53505822cedc13dba6e508f7d1c0d616cca1e339
Dmitry <dfiveg@gmail.com>
2018-08-27 18:58:43 +0300

    Update codeBox.js

--- codeBox.js
+++ codeBox.js
@@ -1,12 +1,12 @@
 (function () {
-    var isMobile = IsMobileDevice(),
-        ahkHref = 'http://forum.script-coding.com/viewforum.php?id=13';
-    
+    var isMobile = isMobileDevice();
+
     configPage();
     configNavBar();
     configCodeBox();
-    CodeParse( getCodeParseParam() );
-    onClickRunCodeButton();
+    CodeParse( getCodeToParse() );
+    if ( isAutoHotkeyPage() ) onClickRunCodeButton();
+    else $('.runCodeButton').each(function() { $(this).hide() });
 
     function configPage() {
         if (isMobile)  {
@@ -24,11 +24,11 @@
         $(".postfoot").css("padding-left", isMobile ? "140px" : "180px");
 
         $("textarea#fld4").css({ fontFamily: "'Droid Sans Mono', monospace",
-                                 height:     isMobile ? "300px" : "640px" });
+                                 height:     isMobile ? "300px" : "640px" });  /* textarea на странице «Персональное» */
     }
 
     function configNavBar() {
-        var $nav = $('#navextra2'), currentLiClass;
+        var $nav = $('#navextra2'), ahkHref = 'http://forum.script-coding.com/viewforum.php?id=13', currentLiClass;
         if (!isMobile) {
             var style = document.getElementById('brd-navlinks').style;
             style.zIndex = 1000;
@@ -75,8 +75,17 @@
                                           fontSize:   "12px" });
     }
 
+    function isAutoHotkeyPage()  {
+        if (isMobile) return false;
+        var found;
+        $('.crumb').each( function () {
+            if ($(this).html().indexOf('AutoHotkey') != -1) return !(found = true);
+        });
+        return found;
+    }
+
     function onClickRunCodeButton() {
-        $(".runCode").mousedown(function()  {
+        $(".runCodeButton").mousedown(function()  {
             $(this).parent().parent().find("code").css("background", "rgb(255, 230, 200)");
         }).mouseup(function()  {
             var $code = $(this).parent().parent().find("code");
@@ -98,13 +107,13 @@
             setTimeout( function() { $(thisButton).css({ transition: 'none', transform: 'none' }) }, 400 );
         }).mouseleave(function()  {
             var $code = $(this).parent().parent().find("code");
-            if ($code.css("background-color") = "rgb(255, 230, 200)")
+            if ($code.css("background-color") == "rgb(255, 230, 200)")
                 $code.css("background", "transparent");
         });
     }
 })();
 
-function getCodeParseParam() {
+function getCodeToParse() {
     return `<div style="margin-top:-4px;
                         margin-bottom:8px;
                         border-bottom:2px solid #acf;
@@ -174,7 +183,7 @@ function getCodeParseParam() {
         HoeSttp4zoex9ndw6sNXQP4rzabjS8hK57Fvu4Yo9hCfTvGV/VcoWp+1KKFbgUr5
         74Yo9mxgFOkj9je06aHJO4pb8AAAAABJRU5ErkJggg==" title="Копировать с 3 пробелами">)%
 
-    <img class="runCode" style="margin-top:4px; cursor:pointer" src="data:image/png;base64,
+    <img class="runCodeButton" style="margin-top:4px; cursor:pointer" src="data:image/png;base64,
         iVBORw0KGgoAAAANSUhEUgAAAAwAAAAMCAYAAABWdVznAAACOklEQVR42lWSX0hT
         cRzFv9/teuca2NRKZ1hJ7lZsDizS2nAr80UoyYcIrLCIfIi91INPvSRKISX00EOy
         MsIibRUVg21RzN2l2D+jJhtimejm1jZ3N7X9u/d3u71EnufzgcM5B9susK69+0sP
@@ -193,7 +202,7 @@ function getCodeParseParam() {
     <br></div>%code%`;
 }
 
-function IsMobileDevice() {
+function isMobileDevice() {
     return (function (a) {
         if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4)))return true
     })(navigator.userAgent || navigator.vendor || window.opera);
